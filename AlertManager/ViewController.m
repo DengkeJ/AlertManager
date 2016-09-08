a50ed59761b58d745358a5887c5ce932d6b8b4d3
DK <DK@dk.local>
2016-09-08 18:28:54 +0800

    Initial Commit

---
+++ AlertManager/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AlertManager
+//
+//  Created by DK on 16/9/8.
+//  Copyright © 2016年 DK. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
